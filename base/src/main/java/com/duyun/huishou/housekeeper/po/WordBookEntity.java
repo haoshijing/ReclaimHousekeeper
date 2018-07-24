dcabcaa082713232bf1dcbf929d7ed6eeb925db8
Squidyu <1297554122@qq.com>
2018-07-25 01:48:40 +0800

    'add:生成mapper'

---
+++ base/src/main/java/com/duyun/huishou/housekeeper/po/WordBookEntity.java
@@ -0,0 +1,63 @@
+package com.duyun.huishou.housekeeper.po;
+
+public class WordBookEntity {
+    private Integer id;
+
+    private String name;
+
+    private String key;
+
+    private String value;
+
+    private Long insertTime;
+
+    private Long lastUpdateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key == null ? null : key.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public Long getInsertTime() {
+        return insertTime;
+    }
+
+    public void setInsertTime(Long insertTime) {
+        this.insertTime = insertTime;
+    }
+
+    public Long getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Long lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+}
